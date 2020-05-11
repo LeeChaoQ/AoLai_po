98388ca481ecd1ba8ac3c4148512385c4cd7ced2
LeeChaoQ <lichao900602@126.com>
2020-05-12 00:56:07 +0800

    初始化

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+from base.base_driver import init_driver
+
+
+class TestLogin:
+
+    def setup(self):
+        self.driver = init_driver()
+
+    def teardown(self):
+        self.driver.quit()
+
+    def test_login(self):
+        pass
